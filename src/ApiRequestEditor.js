70136e1afea899a817d597296da34fb19a799fc2
TwoPlusTwoOne <francisco.digiandomenico@gmail.com>
2019-10-21 17:50:09 -0300

    chore: keep `styles` function, inject into render

--- src/ApiRequestEditor.js
+++ src/ApiRequestEditor.js
@@ -37,96 +37,6 @@ import '@advanced-rest-client/uuid-generator/uuid-generator.js';
  * @memberof ApiElements
  */
 export class ApiRequestEditor extends AmfHelperMixin(EventsTargetMixin(LitElement)) {
-  static get styles() {
-    return [
-      formStyles,
-      css`:host {
-        display: block;
-      }
-
-      .content {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-      }
-
-      .content > * {
-        margin: 0;
-      }
-
-      [hidden] {
-        display: none !important;
-      }
-
-      .panel-warning {
-        width: 16px;
-        height: 16px;
-        margin-left: 4px;
-        color: var(--error-color, #FF7043);
-      }
-
-      .invalid-info {
-        color: var(--error-color);
-        margin-left: 12px;
-      }
-
-      paper-spinner {
-        margin-right: 8px;
-      }
-
-      .action-bar {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        margin-top: 8px;
-      }
-
-      .url-editor {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-      }
-
-      api-url-editor {
-        flex: 1;
-      }
-
-      .section-title {
-        margin: 0.83em 8px;
-        letter-spacing: 0.1rem;
-        font-size: 20px;
-        font-weight: 200;
-      }
-
-      .editor-section {
-        margin: 8px 0;
-      }
-
-      api-body-editor,
-      api-headers-editor,
-      api-url-params-editor,
-      authorization-panel {
-        margin: 0;
-        padding: 0;
-      }
-
-      :host([compatibility]) .section-title {
-        font-size: 18px;
-        font-weight: 400;
-        letter-spacing: initial;
-      }
-
-      :host([narrow]) .content {
-        display: flex;
-        flex-direction: columns;
-      }
-
-      :host([narrow]) api-url-editor {
-        width: auto;
-      }`
-    ];
-  }
-
   static get properties() {
     return {
       /**
@@ -332,6 +242,96 @@ export class ApiRequestEditor extends AmfHelperMixin(EventsTargetMixin(LitElemen
       _endpointUri: { type: String },
       _apiBaseUri: { type: String }
     };
+  }
+
+  get styles() {
+    return [
+      formStyles,
+      css`:host {
+        display: block;
+      }
+
+      .content {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+      }
+
+      .content > * {
+        margin: 0;
+      }
+
+      [hidden] {
+        display: none !important;
+      }
+
+      .panel-warning {
+        width: 16px;
+        height: 16px;
+        margin-left: 4px;
+        color: var(--error-color, #FF7043);
+      }
+
+      .invalid-info {
+        color: var(--error-color);
+        margin-left: 12px;
+      }
+
+      paper-spinner {
+        margin-right: 8px;
+      }
+
+      .action-bar {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-top: 8px;
+      }
+
+      .url-editor {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+
+      api-url-editor {
+        flex: 1;
+      }
+
+      .section-title {
+        margin: 0.83em 8px;
+        letter-spacing: 0.1rem;
+        font-size: 20px;
+        font-weight: 200;
+      }
+
+      .editor-section {
+        margin: 8px 0;
+      }
+
+      api-body-editor,
+      api-headers-editor,
+      api-url-params-editor,
+      authorization-panel {
+        margin: 0;
+        padding: 0;
+      }
+
+      :host([compatibility]) .section-title {
+        font-size: 18px;
+        font-weight: 400;
+        letter-spacing: initial;
+      }
+
+      :host([narrow]) .content {
+        display: flex;
+        flex-direction: columns;
+      }
+
+      :host([narrow]) api-url-editor {
+        width: auto;
+      }`
+    ];
   }
 
   get selected() {
@@ -909,7 +909,7 @@ export class ApiRequestEditor extends AmfHelperMixin(EventsTargetMixin(LitElemen
       _sendLabel,
       _hideParamsEditor
     } = this;
-    return html`
+    return html`<style>${this.styles}</style>
     ${aware ? html`<raml-aware
       .scope="${aware}"
       @api-changed="${this._apiChanged}"></raml-aware>` : ''}
