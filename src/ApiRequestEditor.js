2197403d2b3cdbb1167ac15093dcef0eeed1d87b
Pawel Psztyc <jarrodek@gmail.com>
2020-03-02 11:42:01 -0800

    refactor: moved component styles to a separate file

--- src/ApiRequestEditor.js
+++ src/ApiRequestEditor.js
@@ -11,7 +11,7 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import { html, css, LitElement } from 'lit-element';
+import { html, LitElement } from 'lit-element';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { EventsTargetMixin } from '@advanced-rest-client/events-target-mixin/events-target-mixin.js';
 import { AmfHelperMixin } from '@api-components/amf-helper-mixin/amf-helper-mixin.js';
@@ -29,6 +29,8 @@ import '@anypoint-web-components/anypoint-button/anypoint-icon-button.js';
 import '@polymer/paper-spinner/paper-spinner.js';
 import '@polymer/paper-toast/paper-toast.js';
 import '@advanced-rest-client/uuid-generator/uuid-generator.js';
+import styles from './Styles.js';
+
 /**
  * `api-request-editor`
  *
@@ -250,95 +252,7 @@ export class ApiRequestEditor extends HeadersParserMixin(AmfHelperMixin(EventsTa
   get styles() {
     return [
       formStyles,
-      css`:host {
-        display: block;
-      }
-
-      .content {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-      }
-
-      .content > * {
-        margin: 0;
-      }
-
-      [hidden] {
-        display: none !important;
-      }
-
-      .panel-warning {
-        width: 16px;
-        height: 16px;
-        margin-left: 4px;
-        color: var(--error-color, #FF7043);
-      }
-
-      .invalid-info {
-        color: var(--error-color);
-        margin-left: 12px;
-      }
-
-      paper-spinner {
-        margin-right: 8px;
-      }
-
-      .action-bar {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        align-items: center;
-        margin-top: 8px;
-      }
-
-      .url-editor {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-      }
-
-      api-url-editor {
-        flex: 1;
-      }
-
-      .send-button {
-        white-space: nowrap;
-      }
-
-      .section-title {
-        margin: 0.83em 8px;
-        letter-spacing: 0.1rem;
-        font-size: 20px;
-        font-weight: 200;
-      }
-
-      .editor-section {
-        margin: 8px 0;
-      }
-
-      api-body-editor,
-      api-headers-editor,
-      api-url-params-editor,
-      authorization-panel {
-        margin: 0;
-        padding: 0;
-      }
-
-      :host([compatibility]) .section-title {
-        font-size: 18px;
-        font-weight: 400;
-        letter-spacing: initial;
-      }
-
-      :host([narrow]) .content {
-        display: flex;
-        flex-direction: columns;
-      }
-
-      :host([narrow]) api-url-editor {
-        width: auto;
-      }`
+      styles,
     ];
   }
 
